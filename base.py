b8a020e3eb490bad70a49d51357a6d0a408cd852
Andrew <akuz.dev@gmail.com>
2015-06-28 03:44:28 -0500

    added names

--- base.py
+++ base.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 import twilio.twiml
 #import yaml
 import config
+import requests
+import json
 
 
 
@@ -19,7 +21,7 @@ import config
 #    exit(1)
 
 SECRET_KEY =  config.key #CONFIG['twilio_secret_key']
-#GEOCODE_KEY = CONFIG['geocode_key']
+GEOCODE_KEY = config.geocode_key
 
 app = Flask(__name__)
  
@@ -27,8 +29,23 @@ app = Flask(__name__)
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
     name = request.values.get('Body', None)
+    API_KEY = config.geocode_key
+    Address = name 
+
+    r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + Address + "&key="+API_KEY)
+
+    #print r.json()['results']['geometry']['location']['lat']
+    jsonResponse = json.loads(r.text)
+    latGrab = json.dumps([s['geometry']['location']['lat'] for s in jsonResponse['results']], indent=3)
+    lonGrab = json.dumps([s['geometry']['location']['lat'] for s in jsonResponse['results']], indent=3)
+
+    latitude = latGrab.replace('[', '').replace(']', '').replace(' ', '').replace('\n','')
+    longitude = lonGrab.replace('[', '').replace(']', '').replace(' ', '').replace('\n','')
+    replyString = latitude + "," + longitude
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey" + name)
+    
+
+    resp.message(replyString)
     return str(resp)
  
 if __name__ == "__main__":
