37b9fb086eb3bd4359713efb3224c006a41ec60c
Andrew <akuz.dev@gmail.com>
2015-06-28 07:15:04 -0500

    token commit

--- base.py
+++ base.py
@@ -9,27 +9,16 @@ import json
 
 
 
-# Configuration
-#CONFIG_FILE = 'config.yml'
-
-# Fetch settings from YAML file
-#try:
-#    with open(CONFIG_FILE, 'r') as f:
-#        CONFIG = yaml.load(f)
-#except (OSError, IOError) as e:
-#    print("Please configure config.yml")
-#    exit(1)
-
-SECRET_KEY =  config.key #CONFIG['twilio_secret_key']
+SECRET_KEY =  config.key 
 GEOCODE_KEY = config.geocode_key
 
 app = Flask(__name__)
  
 def helper(obj):
     return obj['approx_dist']
+
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
     name = request.values.get('Body', None)
     medicine = name.split('near')[0]
     location = name.split('near')[1]
@@ -39,14 +28,12 @@ def hello_monkey():
     radius = "100"
 
 
-
     r = requests.get("https://maps.googleapis.com/maps/api/geocode/json?address=" + Address + "&key="+API_KEY)
 
     jsonResponse = json.loads(r.text)
     try:
         latitude = json.dumps(jsonResponse['results'][0]['geometry']['location']['lat'])
         longitude = json.dumps(jsonResponse['results'][0]['geometry']['location']['lng'])
-        
     except:
         replyString = "Location not found. Try another description of the location"
         resp = twilio.twiml.Response()
