b81570264773eec15db3b42667ef1fe637b31331
Andrew <akuz.dev@gmail.com>
2015-06-28 03:10:10 -0500

    added names

--- base.py
+++ base.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
- 
+    name = request.POST.get('Body', '')
     resp = twilio.twiml.Response()
     resp.message("Hello, Mobile Monkey")
     return str(resp)
